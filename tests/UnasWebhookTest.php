ef8a026f1a03b3cad84ecb44f0c4496458c89dd8
Gergely Ignácz <gergely.ignacz@billingo.com>
2024-11-03 02:37:35 +0100

    test: add first skipped tests for CI

---
+++ tests/UnasWebhookTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class UnasWebhookTest extends PHPUnit\Framework\TestCase
+{
+    public function testProcess(): void
+    {
+        $this->markTestSkipped('This test has not been implemented yet.');
+    }
+}
