7c43c3ebe709f0e17bb33d7e4956e72b5aabe728
Gergely Ignácz <gergely.ignacz@billingo.com>
2024-11-06 18:18:08 +0100

    test: add config and unas webhook test

---
+++ tests/ConfigTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Igzard\PhpUnasWebhook\Config;
+
+class ConfigTest extends PHPUnit\Framework\TestCase
+{
+    public function testConfig(): void
+    {
+        $config = Config::init([
+            'hmac' => 'test',
+            'hmac_header' => 'test',
+        ]);
+
+        $this->assertEquals('test', $config->getHmac());
+        $this->assertEquals('test', $config->getHmacHeader());
+    }
+}
